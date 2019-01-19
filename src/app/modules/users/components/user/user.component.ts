7757c520814ea1b0ecf18a3b9ac3280c3038ebf6
saerose <sbarcelocompte@gmail.com>
2019-01-19 19:39:50 +0100

    chore: modified html and sorting by username implemented

--- src/app/modules/users/components/user/user.component.ts
+++ src/app/modules/users/components/user/user.component.ts
@@ -8,7 +8,7 @@ import { ModulesService } from '../../../modules.service';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit{
-  user: User;
+  users: User[];
 
   constructor(
     private modulesService: ModulesService
@@ -20,8 +20,18 @@ export class UserComponent implements OnInit{
 
   getUsers(): void {
     this.modulesService.getUsersFromApi()
-      .subscribe((response: User) => {
-        this.user = response;
+      .subscribe((response: User[]) => {
+        this.users = response.sort((a: User, b: User) => {
+          const aName = a.username.toLowerCase();
+          const bName = b.username.toLowerCase();
+          if (aName > bName) {
+            return 1;
+          } else if (bName > aName) {
+            return -1;
+          }
+
+          return 0;
+        });
       });
   }
 }
