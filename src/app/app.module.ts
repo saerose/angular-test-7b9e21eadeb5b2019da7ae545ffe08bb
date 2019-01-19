0ef47f50061eefd20d75cfd86d32cd90a050a5ef
saerose <sbarcelocompte@gmail.com>
2019-01-19 18:27:51 +0100

    feat: created basic app component html ts and app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Components
+import { AppRoutingModule } from './app-routing.module';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './modules/main/pages/main.component';
+import { PostsComponent } from './modules/posts/pages/posts/posts.component';
+import { UsersComponent } from './modules/users/pages/users/users.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    PostsComponent,
+    UsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
