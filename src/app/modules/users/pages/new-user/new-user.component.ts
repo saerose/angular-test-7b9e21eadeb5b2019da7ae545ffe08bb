e02dc9c37211d2ce9e14df0c0337719d04b18aa4
saerose <sbarcelocompte@gmail.com>
2019-01-19 20:08:31 +0100

    feat: added forms

--- src/app/modules/users/pages/new-user/new-user.component.ts
+++ src/app/modules/users/pages/new-user/new-user.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {IGeo} from "../../../../types";
 
 @Component({
   selector: 'new-user',
   templateUrl: './new-user.component.html',
   styleUrls: ['./new-user.component.scss']
 })
-export class NewUserComponent {}
+export class NewUserComponent implements OnInit {
+  userForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.newUserForm();
+  }
+
+  newUserForm(): void {
+    this.userForm = this.formBuilder.group({
+      name: new FormControl(''),
+      username: new FormControl(''),
+      email: new FormControl(''),
+      street: new FormControl(''),
+      city: new FormControl(''),
+      zipcode: new FormControl(''),
+      phone: new FormControl(''),
+      website: new FormControl('')
+    })
+    this.userForm.valueChanges
+      .subscribe(result => console.log('form', result))
+  }
+}
+
