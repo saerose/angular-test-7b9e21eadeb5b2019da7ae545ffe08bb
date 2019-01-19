47e3a4f669144259c2da4ef9e16c2ff5ac2ddd3d
saerose <sbarcelocompte@gmail.com>
2019-01-19 18:59:38 +0100

    feat: created retrieving function for users

--- src/app/modules/users/components/user/user.component.ts
+++ src/app/modules/users/components/user/user.component.ts
@@ -1,10 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ModulesService } from '../../../modules.service';
 
 @Component({
   selector: 'user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent {
-  @Input() user;
+export class UserComponent implements OnInit{
+  user: object;
+
+  constructor(
+    private modulesService: ModulesService
+  ) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.modulesService.getUsersFromApi()
+      .subscribe(response => {
+        this.user = response;
+      });
+  }
 }
