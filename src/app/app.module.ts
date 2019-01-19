c999a77af923aeacd5b845b6c1d067f894b00723
saerose <sbarcelocompte@gmail.com>
2019-01-19 19:05:23 +0100

    chore: added posts module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 // Components
-import { AppRoutingModule } from './app-routing.module';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './modules/main/pages/main.component';
 import { PostsComponent } from './modules/posts/pages/posts/posts.component';
+
+// Modules
+import { AppRoutingModule } from './app-routing.module';
+import { PostsModule } from './modules/posts/posts.module';
 import { UsersModule } from './modules/users/users.module';
 
 @NgModule({
@@ -21,14 +23,15 @@ import { UsersModule } from './modules/users/users.module';
     HeaderComponent,
     FooterComponent,
     MainComponent,
-    PostsComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    UsersModule
+    UsersModule,
+    PostsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
