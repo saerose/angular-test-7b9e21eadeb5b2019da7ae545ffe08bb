7ccf1681b9308146a38a221ade61abcfb8a54ccf
saerose <sbarcelocompte@gmail.com>
2019-01-20 12:51:17 +0100

    chore: added router

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -1,20 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 // Components
 import { ReactiveFormsModule } from '@angular/forms';
 import { UserComponent } from './components/user/user.component';
-import { NewUserComponent } from './pages/new-user/new-user.component';
 import { UsersComponent } from './pages/users/users.component';
 
 @NgModule({
   declarations: [
     // Components
     UsersComponent,
-    UserComponent,
-    NewUserComponent
+    UserComponent
   ],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule
+  ],
   providers: []
 })
 export class UsersModule {}
